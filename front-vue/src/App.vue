c8ddbdcabcb43c5554ffe86889dab30487496d97
Pavel Groshevskiy <pavel.groshevskiy@gmail.com>
2022-01-31 22:18:28 +0700

    started front vue

---
+++ front-vue/src/App.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <button @click="fetchTable"></button>
+  </div>
+</template>
+
+<script>
+import axios from "axios";
+
+export default {
+  methods: {
+    async fetchTable() {
+      try {
+        const response = await axios.get("localhost:5001/contacts/");
+        console.log(response);
+      } catch (e) {
+        alert("Something go wromg");
+      }
+    },
+  },
+};
+</script>
+
+<style></style>
